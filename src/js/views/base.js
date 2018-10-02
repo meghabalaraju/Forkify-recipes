6cdb6c5346df8cf814bfd34060985824c79ecbbc
meghabalaraju <kbmegha4@outlook.com>
2018-10-02 14:33:21 +0200

    initial commit

---
+++ src/js/views/base.js
@@ -0,0 +1,11 @@
+
+export const key = 'YOUR_API_KEY';
+
+export const baseUrl = `https://www.food2fork.com/api/`;
+
+export const elements = {
+  searchForm: document.querySelector('.search'),
+  searchInput: document.querySelector('.search__field'),
+  searchResultList: document.querySelector('.results__list'),
+  recipeList: document.querySelector('.recipe')
+}
